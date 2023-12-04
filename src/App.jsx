79a44416ae0323592ee495cb1fbced7b9d44e1da
Danbi Choi <sweetrain05@hotmail.com>
2023-12-03 16:18:51 -0800

    Header, UserInput updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import UserInput from './components/UserInput';
+import Result from './components/Result';
+
 function App() {
-  return (
-    <h1>React Investment Calculator</h1>
-  )
+    const [userInput, setUserInput] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1200,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    const handleUserInputChange = (identifier, inputValue) => {
+        setUserInput((prev) => ({ ...prev, [identifier]: inputValue }));
+    };
+
+    return (
+        <>
+            <Header />
+            <UserInput onChange={handleUserInputChange} userInput={userInput} />
+            <Result userInput={userInput} />
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Result.jsx
@@ -0,0 +1,6 @@
+const Result = ({ userInput }) => {
+    console.log(userInput);
+    return <section id='result'>Result</section>;
+};
+
+export default Result;
